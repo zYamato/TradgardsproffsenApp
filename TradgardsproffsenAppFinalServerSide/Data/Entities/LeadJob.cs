48b474040d79539e112402359e7d21ee188bc755
Jesper Eriksson <jesper.eriksson.99@outlook.com>
2021-02-11 11:10:56 +0100

    Made changes to make LostLead Work (Not Done)

--- TradgardsproffsenAppFinalServerSide/Data/Entities/LeadJob.cs
+++ TradgardsproffsenAppFinalServerSide/Data/Entities/LeadJob.cs
@@ -9,8 +9,11 @@ namespace TradgardsproffsenApp.Entities
         [Required]
         [Key]
         public int Id { get; set; }
+        public ValidatedLead ValidatedLead { get; set; }
         public int? ValidatedLeadId { get; set; }
+        public SentOutLead SentOutLead { get; set; }
         public int? SentOutLeadId { get; set; }
+        public Job Job { get; set; }
         public int JobId { get; set; }
     }
 }
---
+++ TradgardsproffsenAppFinalServerSide/Pages/LoseLeadBase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TradgardsproffsenApp.Data.Services;
+
+namespace TradgardsproffsenApp.Pages
+{
+    public class LoseLeadBase : ComponentBase
+    {
+
+        public Entities.Lead lead { get; set; } = new Entities.Lead();
+        public Entities.ValidatedLead validLead { get; set; } = new Entities.ValidatedLead();
+        public Entities.SentOutLead aentOutLead { get; set; } = new Entities.SentOutLead();
+
+
+
+        [Inject]
+        public LeadService leadsService { get; set; }
+
+        [Inject]
+        public ValidatedLeadService validatedLeadService { get; set; }
+
+
+        [Parameter]
+        public string Id { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            lead = await leadsService.GetLeadByID(int.Parse(Id));
+            validLead = await validatedLeadService.GetValidatedLeadByID(int.Parse(Id));
+        }
+    }
+}
